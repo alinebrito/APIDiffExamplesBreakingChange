139a543f9fd0c059a03636f6de481456daaecfee
Aline Brito <nbrito.aline@gmail.com>
2017-03-04 20:17:19 -0300

    Métodos removidos para teste.

--- src/main/java/br/ufmg/dcc/labsoft/methods/delete/Method8.java
+++ src/main/java/br/ufmg/dcc/labsoft/methods/delete/Method8.java
@@ -4,23 +4,4 @@ import java.io.IOException;
 
 public class Method8 {
 
-	@Deprecated
-	public void m1() throws IllegalArgumentException{
-		
-	}
-	
-	@Deprecated
-	private void m2() throws IOException{
-		
-	}
-	
-	@Deprecated
-	protected void m3() throws ArithmeticException{
-		
-	}
-	
-	@Deprecated
-	void m4() throws ArithmeticException{
-		
-	}
 }
--- src/main/java/br/ufmg/dcc/labsoft/methods/delete/Method9.java
+++ src/main/java/br/ufmg/dcc/labsoft/methods/delete/Method9.java
@@ -4,20 +4,5 @@ import java.io.IOException;
 
 @Deprecated
 public class Method9 {
-
-	public void m1() throws IllegalArgumentException{
-		
-	}
 	
-	private void m2() throws IOException{
-		
-	}
-	
-	protected void m3() throws ArithmeticException{
-		
-	}
-	
-	void m4() throws ArithmeticException{
-		
-	}
 }
