f2101e0dbf86694482b34e8f45cb8905d5e17fc7
Aline Brito <nbrito.aline@gmail.com>
2017-03-05 23:00:48 -0300

    Class de teste de unidade adicionada.

---
+++ src/test/java/br/ufmg/dcc/labsoft/modifierfinal/ModifierFinal.java
@@ -0,0 +1,11 @@
+package br.ufmg.dcc.labsoft.modifierfinal;
+
+import junit.framework.TestCase;
+
+public class ModifierFinal extends TestCase {
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+}
