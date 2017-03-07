c2598166177cb0422e85c821de5cb0b2ff844aa8
Aline Brito <nbrito.aline@gmail.com>
2017-03-07 14:52:24 -0300

    Non-API de teste adicionada.

---
+++ src/test/java/br/ufmg/dcc/labsoft/methods/delete/Method7Test.java
@@ -0,0 +1,11 @@
+package br.ufmg.dcc.labsoft.methods.delete;
+
+import junit.framework.TestCase;
+
+public class Method7Test extends TestCase {
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+}
