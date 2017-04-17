680d37f535df403fdb2f60d8c2da9f496e2484fe
Aline Brito <nbrito.aline@gmail.com>
2017-04-17 06:57:55 -0300

    Anotações adicionadas para teste.

---
+++ src/main/java/br/ufmg/dcc/labsoft/annotations/Annotation5.java
@@ -0,0 +1,17 @@
+package br.ufmg.dcc.labsoft.annotations;
+
+public @interface Annotation5{
+	
+	String created();
+	
+	int createdd();
+	
+	String createBy();
+	
+	String lastModified();
+	
+	String lastModifiedBy();
+	
+	int cod();
+
+}
---
+++ src/main/java/br/ufmg/dcc/labsoft/annotations/Annotation7.java
@@ -0,0 +1,17 @@
+package br.ufmg.dcc.labsoft.annotations;
+
+public @interface Annotation7{
+	
+	String created();
+	
+	int createdd();
+	
+	String createBy();
+	
+	String lastModified();
+	
+	String lastModifiedBy();
+	
+	int cod();
+
+}
