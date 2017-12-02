f5adac57d0fc2878a8d343494a42cca1e6df8c05
Aline Brito <nbrito.aline@gmail.com>
2017-12-02 21:05:20 -0200

    Remove default value.

--- src/main/java/br/ufmg/dcc/labsoft/fields/Field4.java
+++ src/main/java/br/ufmg/dcc/labsoft/fields/Field4.java
@@ -5,6 +5,6 @@ public class Field4 extends FieldSuperClass5{
 	//VALUE_DEFAULT_11 --> VALUE_DEFAULT_22
 	public String x2 = "VALUE_DEFAULT_22";
 	
-	private int teste = 123;
+	private int teste;
 
 }
