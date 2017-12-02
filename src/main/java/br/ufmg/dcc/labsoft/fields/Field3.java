ad4c8d91a1a170ad8bd0560ce1a400f568fd53fe
Aline Brito <nbrito.aline@gmail.com>
2017-12-02 20:37:42 -0200

    Move field.

--- src/main/java/br/ufmg/dcc/labsoft/fields/Field3.java
+++ src/main/java/br/ufmg/dcc/labsoft/fields/Field3.java
@@ -7,9 +7,6 @@ public class Field3 {
 	private int x1;
 	
 	//VALUE_DEFAULT_11 --> VALUE_DEFAULT_22
-	public String x2 = "VALUE_DEFAULT_22";
-	
-	//VALUE_DEFAULT_11 --> VALUE_DEFAULT_22
 	@Deprecated
 	public String x3 = "VALUE_DEFAULT_22";
 
--- src/main/java/br/ufmg/dcc/labsoft/fields/Field4.java
+++ src/main/java/br/ufmg/dcc/labsoft/fields/Field4.java
@@ -2,4 +2,7 @@ package br.ufmg.dcc.labsoft.fields;
 
 public class Field4 {
 
+	//VALUE_DEFAULT_11 --> VALUE_DEFAULT_22
+	public String x2 = "VALUE_DEFAULT_22";
+
 }
